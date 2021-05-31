609d6ae26d5dc72668e9bb8e41df17c37a3fe0c6
Saleh Ahmed <46069811+thissaleh@users.noreply.github.com>
2021-06-01 00:36:53 +0800

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import { Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import contentData from "../utils/contentData";
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="next-steps my-5">
+        
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    <div className="logo" />
+    <p>
+    © 2021 British American Tobacco Malaysia. All Rights Reserved. <a href="https://auth0.com"></a>
+    </p>
+  </footer>
+);
+
+export default Footer;
