49ed624c335ef2c530cb29bd15c896ba1752feaf
Saleh Ahmed <46069811+thissaleh@users.noreply.github.com>
2021-06-10 12:26:58 +0800

    fff

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import logo from "../assets/logo.png";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4"> BAT Cloud AAutomation</h1>
+    <h1 className="mb-4"> BAT Cloud Automation</h1>
 
     <p className="lead">
         <a href="">BAT CLoud Self-Service</a>
